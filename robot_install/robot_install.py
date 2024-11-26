90eac900c872bea171ad32d6eb5b11d0978a2f52
angelalonso <alonsofonseca.angel@gmail.com>
2024-11-26 22:28:49 +0100

    feat: preparing Robot, pt.1 3

--- robot_install/robot_install.py
+++ robot_install/robot_install.py
@@ -35,8 +35,13 @@ def main():
 #    sudo dd if=20231109_raspi_3_bookworm.img of=/dev/mmcblk0 status=progress
 # Mounted at /media/aaf/RASPIFIRM/ and /media/aaf/RASPIROOT ? 
 # touch /media/aaf/RASPIFIRM/ssh
+# Do you have a key pair? do you want to create it?
+# sudo vim /media/aaf/RASPIFIRM/sysconf.txt
+# root_authorized_key=ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAINmEzgdeRcX5iGbVmeYT7X0fyDzAh58juL6xYAwyPHCR alonsofonseca.angel@gmail.com
+# save, connect to raspberry
 # Connect raspi with RJ45, find it
-# ssh pi@<IP> - raspberry
+# nmap -sP 192.168.1.0/24 - ask if needs to be used, then get list before starting, then compare and get the new IP, asking for confirmation step by step
+# ssh pi@<IP> - raspberry doesnt work
 
 
 if __name__ == "__main__":
