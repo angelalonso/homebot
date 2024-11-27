4d289dc37699b28bd20adbdeade8caa723687e8b
Angel Alonso Fonseca <angel.alonso.fonseca@westernacher.com>
2024-11-27 09:56:11 +0100

    feat: improving install to raspi

--- robot_install/robot_install.py
+++ robot_install/robot_install.py
@@ -30,14 +30,28 @@ def main():
     logger.info("Requirements:\n\
             - Now run balena-etcher")
     logger.info("ALL DONE.")
+#- MicroSD Card, I have used 16G but maybe 8G is enough.
+#- Raspberry pi 3B+
+#1. Download https://raspi.debian.net/tested/20231109_raspi_3_bookworm.img.xz from https://raspi.debian.net/tested-images/ 
+#1. unxz 20231109_raspi_3_bookworm.img.xz
     # Do you have a microSD with Debian ARMHF installed?
     # Do you want to do it now?
 #    sudo dd if=20231109_raspi_3_bookworm.img of=/dev/mmcblk0 status=progress
 # Mounted at /media/aaf/RASPIFIRM/ and /media/aaf/RASPIROOT ? 
 # touch /media/aaf/RASPIFIRM/ssh
 # Do you have a key pair? do you want to create it?
+### OPTION 1 -DID NOT WORK
 # sudo vim /media/aaf/RASPIFIRM/sysconf.txt
 # root_authorized_key=ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAINmEzgdeRcX5iGbVmeYT7X0fyDzAh58juL6xYAwyPHCR alonsofonseca.angel@gmail.com
+### OPTION 2 -CHROOT
+# sudo apt install qemu qemu-user-static binfmt-support
+# chmod +x chroot_pi.sh
+#     Thanks to https://gist.github.com/htruong/7df502fb60268eeee5bca21ef3e436eb
+# ./chroot_pi.sh /dev/mmcblk0
+# vi /etc/ssh/sshd_config
+#   PermitRootLogin yes
+
+### Common
 # save, connect to raspberry
 # Connect raspi with RJ45, find it
 # nmap -sP 192.168.1.0/24 - ask if needs to be used, then get list before starting, then compare and get the new IP, asking for confirmation step by step
