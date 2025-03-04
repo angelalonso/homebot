f0fc51a44b4ef17a9bb595d13041a2e6e53dde8d
Angel Alonso Fonseca <angel.alonso.fonseca@westernacher.com>
2025-03-04 13:25:14 +0100

    feat: move to remote

--- homebotctl/src/remote.rs
+++ homebotctl/src/remote.rs
@@ -12,34 +12,80 @@ pub fn deploy(
     local_file_path: &str,
     remote_file_path: &str,
 ) -> Result<String, String> {
-    let run = |cmd: &str| run_over_ssh(host, port, username, password, ssh_key_path, cmd);
-    let copy = || {
-        copy_file_over_ssh(
-            host,
-            port,
-            username,
-            password,
-            ssh_key_path,
-            &local_file_path,
-            &remote_file_path,
-        )
-    };
-
+//    let run = |cmd: &str| run_over_ssh(host, port, username, password, ssh_key_path, cmd);
+//    let copy = || {
+//        copy_file_over_ssh(
+//            host,
+//            port,
+//            username,
+//            password,
+//            ssh_key_path,
+//            &local_file_path,
+//            &remote_file_path,
+//        )
+//    };
+//
+//    println!("Cleaning up previous binary...");
+//    let mut comm_rm = "rm ".to_owned();
+//    comm_rm.push_str(&remote_file_path);
+//    let run_comm_rm = run_over_ssh(host, port, username, password, ssh_key_path, &comm_rm);
+//    println!("Result: {:#?}", run_comm_rm);
+//
+//    println!("Copying binary to Bot...");
+//    if let Ok(msg) = copy() {
+//        println!("Result: {:#?}", msg);
+//        println!("Running a test...");
+//        let run_comm_chmod = run(&format!("chmod +x {}", remote_file_path));
+//        println!("{:#?}", run_comm_chmod);
+//    } else {
+//        println!("ERROR SCP'ing to the host");
+//    }
+//
+//    Ok("Deployment completed".to_string())
     println!("Cleaning up previous binary...");
     let mut comm_rm = "rm ".to_owned();
     comm_rm.push_str(&remote_file_path);
-    let run_comm_rm = run_over_ssh(host, port, username, password, ssh_key_path, &comm_rm);
+    let run_comm_rm = run_over_ssh(
+        host,
+        port,
+        username,
+        password,
+        ssh_key_path,
+        &comm_rm,
+    );
+    // TODO: test result, stop if error
     println!("Result: {:#?}", run_comm_rm);
-
     println!("Copying binary to Bot...");
-    if let Ok(msg) = copy() {
-        println!("Result: {:#?}", msg);
-        println!("Running a test...");
-        let run_comm_chmod = run(&format!("chmod +x {}", remote_file_path));
-        println!("{:#?}", run_comm_chmod);
-    } else {
-        println!("ERROR SCP'ing to the host");
+    match copy_file_over_ssh(
+        host,
+        port,
+        username,
+        password,
+        ssh_key_path,
+        &local_file_path,
+        &remote_file_path,
+    ) {
+        Ok(msg) => {
+            println!("Result: {:#?}", msg);
+            println!("Running a test...");
+            let mut comm_chmod = "chmod +x ".to_owned();
+            comm_chmod.push_str(&remote_file_path);
+            let run_comm_chmod = run_over_ssh(
+                host,
+                port,
+                username,
+                password,
+                ssh_key_path,
+                &comm_chmod,
+            );
+            println!("{:#?}", run_comm_chmod);
+            Ok("OK".to_string())
+            // TODO: what else is needed here? start system service maybe? -> need
+            // previous step to deploy it
+        },
+        Err(e) => {
+            println!("ERROR SCP'ing to the host: {:#?}", e);
+            Err("ERROR".to_string())
+        }
     }
-
-    Ok("Deployment completed".to_string())
 }
